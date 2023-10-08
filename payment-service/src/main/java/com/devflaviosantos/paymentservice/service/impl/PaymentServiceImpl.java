7f09f757cae9a8266bef0ff2d2c6c840cba83455
Flavio Santos <devflaviosantos@gmail.com>
2023-10-08 08:42:55 -0300

    add producer config

--- payment-service/src/main/java/com/devflaviosantos/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/devflaviosantos/paymentservice/service/impl/PaymentServiceImpl.java
@@ -1,20 +1,35 @@
 package com.devflaviosantos.paymentservice.service.impl;
 
+import java.io.Serializable;
+
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.devflaviosantos.paymentservice.model.Payment;
 import com.devflaviosantos.paymentservice.service.PaymentService;
 
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
+@RequiredArgsConstructor
 @Service
 public class PaymentServiceImpl implements PaymentService{
+	
+	
+	private final KafkaTemplate<String, Serializable> kafkaTemplate;
 
+	@SneakyThrows
 	@Override
 	public void sendPayment(Payment payment) {
 		// TODO Auto-generated method stub
 		log.info("PAYMENT_SERVICE_IMPL >>> Recebi o Pagamento {}", payment);
+		Thread.sleep(1000);
+		
+		log.info("Enviando pagamento....");
+		kafkaTemplate.send("payment-topic", payment);
+		
 		
 	}
 
